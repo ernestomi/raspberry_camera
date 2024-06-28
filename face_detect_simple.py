7098dc75c7b04696a28a4368b59601c56b9995a3
Ernesto Monroy <ehmizmg@gmail.com>
2024-06-28 12:59:07 +0100

    debug face detection finding classifier

--- face_detect_simple.py
+++ face_detect_simple.py
@@ -22,9 +22,7 @@ def process_frame(frame):
  return gray
 
 # main ------------------------------------------------------------------------
-face_classifier = cv2.CascadeClassifier(
- cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-)
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 while True:
  target_found = False
