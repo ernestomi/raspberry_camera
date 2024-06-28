cd37ec69ace30413ed7f0b530aaf250bb3c83317
Ernesto Monroy <ehmizmg@gmail.com>
2024-06-28 12:10:15 +0100

    one save per frame

--- main.py
+++ main.py
@@ -44,11 +44,12 @@ while True:
   (x, y, w, h) = cv2.boundingRect(c)
   # draw the bounding box on the frame
   cv2.rectangle(threshold, (x, y), (x + w, y + h), (0, 255, 0), 2)
-  #Save the images
-  folder = 'data/{}'.format(datetime.now().strftime('%Y%m%d%H%M%S%f'))
-  os.makedirs(folder)
-  cv2.imwrite('{}/1-current_frame.jpg'.format(folder), current_frame)
-  cv2.imwrite('{}/2-previous_frame.jpg'.format(folder), previous_frame)
-  cv2.imwrite('{}/3-delta.jpg'.format(folder), delta)
-  cv2.imwrite('{}/4-threshold.jpg'.format(folder), threshold)
+ 
+ #Save the images
+ folder = 'data/{}'.format(datetime.now().strftime('%Y%m%d%H%M%S%f'))
+ os.makedirs(folder)
+ cv2.imwrite('{}/1-current_frame.jpg'.format(folder), current_frame)
+ cv2.imwrite('{}/2-previous_frame.jpg'.format(folder), previous_frame)
+ cv2.imwrite('{}/3-delta.jpg'.format(folder), delta)
+ cv2.imwrite('{}/4-threshold.jpg'.format(folder), threshold)
  
